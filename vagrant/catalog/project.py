d1a9fcd96a8cc1f74d02bf9c9f72b71daba3b51b
Anuj Syal <syal.anuj@gmail>
2016-12-11 17:08:53 +0530

    catalog.json

--- vagrant/catalog/project.py
+++ vagrant/catalog/project.py
@@ -189,25 +189,16 @@ def gdisconnect():
         return response
 
 
-# # JSON APIs to view Restaurant Information
-# @app.route('/restaurant/<int:restaurant_id>/menu/JSON')
-# def restaurantMenuJSON(restaurant_id):
-#     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-#     items = session.query(MenuItem).filter_by(
-#         restaurant_id=restaurant_id).all()
-#     return jsonify(MenuItems=[i.serialize for i in items])
-
-
-# @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/JSON')
-# def menuItemJSON(restaurant_id, menu_id):
-#     Menu_Item = session.query(MenuItem).filter_by(id=menu_id).one()
-#     return jsonify(Menu_Item=Menu_Item.serialize)
-
-
-# @app.route('/restaurant/JSON')
-# def restaurantsJSON():
-#     restaurants = session.query(Restaurant).all()
-#     return jsonify(restaurants=[r.serialize for r in restaurants])
+# JSON APIs to view Item Information
+@app.route('/catalog.json')
+def catalogJSON():
+    categories = session.query(Category).order_by(asc(Category.name)).all()
+    items = session.query(Item).order_by(asc(Item.name)).all()
+    # for category in categories:
+    #     for item in items:
+    #         if category.id == item.category_id:
+    #             category.items.append(item)
+    return jsonify(Categories=[i.serialize for i in categories], Items = [i.serialize for i in items])
 
 
 # Show all categories and latest items
